081337e700827321d7b7ee3a61b257b3f5fc54bf
DaveTD <its.dave.dawson@gmail.com>
2016-01-21 11:16:22 -0500

    First draft for getting arrays to work

--- lib/superstudio.rb
+++ lib/superstudio.rb
@@ -1,7 +1,6 @@
 module Superstudio
   class SqlJsonBuilder
-
-    attr_accessor :sql_columns, :row_being_used, :json_result, :schema, :template_body, :template_bodies, :template_types, :array_paths, :json_nodes
+    attr_accessor :sql_columns, :row_being_used, :json_result, :schema, :template_bodies, :template_types, :array_paths, :json_nodes
 
     def initialize(query, file_name = nil)
       file_class_name = self.class.name
@@ -9,9 +8,7 @@ module Superstudio
       file_name ||= file_class_name.snakecase << ".json.schema"
 
       @schema = parse_json_schema(file_name)
-      @sql_columns = []
-      @row_being_used = []
-      @array_paths = [] # a path name points to a depth so that we can build from the deepest up
+      @sql_columns, @row_being_used, @array_paths = [], [], []
       @json_result = ""
       @json_nodes = {}
 
@@ -19,9 +16,10 @@ module Superstudio
 
       if query.present?
         result_set = get_sql_results(query)
+        create_template(json_schema_interpretation)
         assemble_json(result_set, json_schema_interpretation)
       else
-        @template_body, @template_types = create_template(json_schema_interpretation)
+        create_template(json_schema_interpretation)
       end
     end
 
@@ -34,9 +32,7 @@ module Superstudio
       end
     end
 
-    # Get the file contents as a string and interpret as JSON
     def parse_json_schema(file_name)
-    # version 0.4.0 will need to nest referenced schema pieces into the file_location's schema here
       directory = schemas_directory
       master_file = directory.join(file_name)
       contents = File.read(master_file)
@@ -45,44 +41,18 @@ module Superstudio
     end
 
     def get_sql_results(query)
-      # if this is an AR query, find the resulting query that would be run
-      if query.respond_to?(:to_sql)
-        query = query.to_sql
-      end
-
+      query = query.to_sql if query.respond_to?(:to_sql)
       result_set = ActiveRecord::Base.connection.select_all(query)
     end
 
-    # Convenience for finding the row value for the column name
     def value_by_column_name(name)
       return @row_being_used[@sql_columns.index(name)]
     end
 
-    # Convenience for finding the row value for a column number - in case people went and had multiple columns named the same thing
     def value_by_column_number(number)
       return @row_being_used[number]
     end
 
-    # inserting to the json_result, figuring out if this is the first item in the json_result or not first
-    def insert_json_to_response(template, row_hash)
-      new_response_node = template % row_hash
-      if @json_result.empty?
-        @json_result << new_response_node
-      else
-        @json_result << ",#{new_response_node}"
-      end
-    end
-
-    #  #####################  #
-    #  Parsing the JSON spec  #
-    #  #####################  #
-
-    # Search for "type" nodes
-    # A type of "object" denotes a branch
-    # A type of "array" indicates an array
-    # All other types are expected columns
-    # For non-object types, find the parent node, that will be the related name
-    # Almost everything else can be ignored
     def interpret_json_schema(json_hash, depth = 0, path_array = [], expected_mappings = [], node_name = 'root')
       json_hash = replace_reference_keys(json_hash)
       level_keys = json_hash.keys
@@ -104,15 +74,11 @@ module Superstudio
         end
 
         if key == "items"
-          # when we find items, we need to indicate that this is an array, and that the types that follow can be
-          # repeated multiple times.
           new_path_array = path_array.dup
           new_path_array << node_name
           array_paths.push(new_path_array)
-          #new_name = node_name.dup << '_array'
           expected_mappings << {depth: depth, path: path_array, name: node_name, node_type: json_hash[key]}
         end
-
       end
       return expected_mappings.flatten
     end
@@ -122,7 +88,6 @@ module Superstudio
         # split on a hash character - the first piece is the file name
         file_name = json_hash["$ref"].split('#')[0]
         referenced_contents = parse_json_schema(file_name)
-        # replace reference keys in the referenced contents
         referenced_contents = replace_reference_keys(referenced_contents)
         json_hash.delete("$ref")
         json_hash = json_hash.merge(referenced_contents)
@@ -130,27 +95,13 @@ module Superstudio
       return json_hash
     end
 
-    # Generate a string JSON template like:
-    # {
-    #   "firstName": %{root_firstName},
-    #   "lastName": %{root_lastName},
-    #   "age": %{root_age},
-    #   "foo": {
-    #     "bar": %{root_foo_bar},
-    #     "bin": %{root_foo_bin}
-    #   }
-    # }
-    # Each row returned from an SQL query will be used to populate these variables in a hash
-    # There is a chance that using more c-like notation like %s and %i would improve speeds
-    # Need to test out a mapping between these templates and an ordering of items instead to
-    # see if performance improves
+
     def create_template(expected_mappings)
       fork_nodes = expected_mappings.uniq { |i| i[:path] }
       max_depth = expected_mappings.max_by { |x| x[:depth] }[:depth]
       nested_objects = {}
 
       template_nodes = []
-      @template_body = ""
       @template_types = {}
 
       @template_bodies = {}
@@ -158,7 +109,6 @@ module Superstudio
         @template_bodies[array_path] = ""
       end
 
-      # go through each depth, building upwards
       while max_depth != 0
         objects_at_depth = fork_nodes.select {|j| j[:depth] == max_depth}
         objects_at_depth.each do |object|
@@ -179,8 +129,7 @@ module Superstudio
               object_string << "#{nested_object[1]},"
             end
           end
-          # cut off the last "," from the string
-          object_string = object_string[0..-2]
+          object_string = object_string.chomp(",")
           object_string << "}"
           # if this isn't the root node, put it in the nested_objects hash
           if max_depth > 1
@@ -190,7 +139,6 @@ module Superstudio
               nested_objects[object[:path]] = object_string
             end
           else
-            @template_body = object_string
             @template_bodies[['root']] = object_string
           end
         end
@@ -198,69 +146,95 @@ module Superstudio
       end
     end
 
-    #  #####################  #
-    #  Parsing the JSON spec  #
-    #  #####################  #
-
-    #  ###################  #
-    #  Assembling the JSON  #
-    #  ###################  #
-
     def assemble_json(result_set, expected_mappings)
-      create_template(expected_mappings)
       @sql_columns = result_set.columns
-
-      # we need a set of row hashes to which new rows can have their array items added
-      # example:
-      # array_paths = [['root'], ['root', 'variants'], ['root', 'variants', 'tax_category']]
-      #
-      # 1. Find the deepest path (root, variants, tax_category)
-      # 2. Find the associated keys to that path (variants_tax_category_id, variants_tax_category_name)
-      # 3. Find the associated keys to its parent and up (id, foo, variants_id, variants_name)
+      complete_templates = Hash.new{|hash, key| hash[key] = Hash.new}
 
       # map row comes from the inheriting class
       result_set.rows.each do |row|
-        @json_nodes = {}
         @row_being_used = row
+        @json_nodes = {}
         map_row
-        # based on template types, determine what needs to go in quotes and what needs to be called "null"
         row_hash = clean_data_types(@json_nodes, @template_types)
+        key_depth = 0
+        # Sort the template bodies so that the longest keys come first
+        @template_bodies = @template_bodies.sort{ |k,v| k.length }.reverse.to_h
 
-        # Slap the cleaned hash onto each template body
+        @template_bodies.each do |key, template|
+          key_eliminator = ''
+          working_hash = row_hash.dup
+          local_values = {}
+          # Find the depth of this key
+          key_depth = key.length
+          # Find all other keys of that depth
+          keys_at_depth = @template_bodies.select{ |k,v| k.length == key_depth }
+          # Eliminate variables of that depth, or deeper
+          keys_at_depth.keys.each do |depth_key|
+            key_eliminator = depth_key.join('_')
+            local_values = working_hash.select { |k,v| k.to_s.start_with?(key_eliminator) }
+            working_hash.delete_if { |k,v| k.to_s.start_with?(key_eliminator)}
+          end
 
+          # Key on the remaining values
+          valued_key = ""
+          working_hash.each do |key, value|
+            valued_key << value.to_s << "_"
+          end
+          valued_key = valued_key.chomp("_").to_sym
 
-        # Sort through the template bodies to figure out where each should go
+          # set the array key value for this row to the new, generated key, as a replacable template
+          # this goes in the row_hash, because it will be copied down into further templates with shorter keys
+          # where the key_eliminator is this array (for this row only)
+          row_hash[key_eliminator.to_sym] = "[%{#{valued_key}}]"
 
-        insert_json_to_response(@template_body, row_hash)
+          # fill in the array values for the row_hash
+          complete_template = template % local_values
+
+          # Merge this template with any other templates of that key - this is another item in that array
+          if complete_templates[key_depth][valued_key].present?
+            if !complete_templates[key_depth][valued_key].include?(complete_template)
+              complete_templates[key_depth][valued_key] = complete_templates[key_depth][valued_key] << "," << complete_template
+            end
+          else
+            complete_templates[key_depth][valued_key] = complete_template
+          end
+        end
       end
+      # Merge templates together
+      max_depth = complete_templates.max_by{|k,v| k}.first
+      while max_depth > 0
+        complete_templates[max_depth].each do |k,v|
+          complete_templates[max_depth][k] = complete_templates[max_depth][k] % complete_templates[max_depth + 1]
+        end
+        max_depth -= 1
+      end
+      @json_result << complete_templates[1].values.join(',')
     end
 
     def clean_data_types(row_hash, template_types)
       row_hash.keys.each do |key|
         if template_types[key] == "string" && row_hash[key] != nil
           row_hash[key] = "\"#{row_hash[key]}\""
+          row_hash[key] = row_hash[key].gsub("%", "%%")
         end
         row_hash[key] = "null" if row_hash[key].nil?
       end
       return row_hash
     end
-
-    #  ###################  #
-    #  Assembling the JSON  #
-    #  ###################  #
   end
 end
 
 # Development Plan
 
-# version 0.1.x - single schema works
-# version 0.2.x - use mapping files to map from result set to schema
-# version 0.3.x - allow n-depth schemas, correctly handle nulls
-# version 0.4.x - allow "$ref" to nest schemas
-# version 0.5.x - testing types between result set and expected schema values
 # version 0.6.x - support arrays of objects
 # version 0.7.x - automatically generate data maps with all expected variables
 # version 0.8.x - support all settings for current draft of json schema
 # version 1.0.x - test suite
 
 # At some point I will address the problem of referencing schemas that define a node to be used, and of using references that are not on the same server
+# Each row returned from an SQL query will be used to populate these variables in a hash
+    # There is a chance that using more c-like notation like %s and %i would improve speeds
+    # Need to test out a mapping between these templates and an ordering of items instead to
+    # see if performance improves
+# Eventually I'll take a look at having variable keys
+# Eventually I'll look at performance for replacing all this array/hash/string stuff with a few tree structure objects
